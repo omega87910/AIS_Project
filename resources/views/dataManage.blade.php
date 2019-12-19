a11b12a33c0cd794a2a46178f149a4b4e4ba2fcf
omega87910 <106021054@live.asia.edu.tw>
2019-12-19 14:51:46 +0800

    已可以在管理區修改資料

--- resources/views/dataManage.blade.php
+++ resources/views/dataManage.blade.php
@@ -16,11 +16,20 @@ if (isset($_GET['search_keyword'])){
     }
 }else if(isset($_GET['delete'])){
     $delete = $_GET['delete'];
-    echo $delete;
     DB::delete("delete from data_list where id = ?",[$delete]);
 }else if (isset($_GET['modify'])){
     $modify = $_GET['modify'];
-    echo $modify;
+    $new_main_keyword = $_GET['new_main_keyword'];
+    $new_second_keyword = $_GET['new_second_keyword'];
+    $new_product_description = $_GET['new_product_description'];
+    $new_price = $_GET['new_price'];
+    $new_color = $_GET['new_color'];
+    $new_part = $_GET['new_part'];
+    $new_thickness = $_GET['new_thickness'];
+    $new_size = $_GET['new_size'];
+    $new_instruction_for_use = $_GET['new_instruction_for_use'];
+    $new_instruction_for_others = $_GET['new_instruction_for_others'];
+    DB::table('data_list')->where(['id'=>$modify])->update(['main_keyword'=>$new_main_keyword,'second_keyword'=>$new_second_keyword,'product_description'=>$new_product_description,'price'=>$new_price,'color'=>$new_color,'part'=>$new_part,'thickness'=>$new_thickness,'size'=>$new_size,'instruction_for_use'=>$new_instruction_for_use,'instruction_for_others'=>$new_instruction_for_others]);
 }
 ?>
 <div style="padding:0% 25% 0% 25%">
@@ -52,25 +61,33 @@ if (isset($_GET['search_keyword'])){
                 <th>功能</th>
             </tr>
         </thead>
-        <tbody>
+        <tbody style="font-size:14px">
             @foreach($datalists as $datalist)
             <tr>
-                <td>{{$datalist->main_keyword}}</td>
-                <td>{{$datalist->second_keyword}}</td>
-                <td>{{$datalist->product_description}}</td>
+                <form>
+                <td><input name="new_main_keyword" value={{$datalist->main_keyword}}></td>
+                <td><input name="new_second_keyword" value={{$datalist->second_keyword}}></td>
+                <td><input name="new_product_description" value={{$datalist->product_description}}></td>
+                <td><input name="new_price" value={{$datalist->price}}></td>
+                <td><input name="new_color" value={{$datalist->color}}></td>
+                <td><input name="new_part" value={{$datalist->part}}></td>
+                <td><input name="new_thickness" value={{$datalist->thickness}}></td>
+                <td><input name="new_size" value={{$datalist->size}}></td>
+                <td><input name="new_instruction_for_use" value={{$datalist->instruction_for_use}}></td>
+                <td><input name="new_instruction_for_others" value={{$datalist->instruction_for_others}}></td>
+                {{-- <td>{{$datalist->product_description}}</td>
                 <td>{{$datalist->price}}</td>
                 <td>{{$datalist->color}}</td>
                 <td>{{$datalist->part}}</td>
                 <td>{{$datalist->thickness}}</td>
                 <td>{{$datalist->size}}</td>
                 <td>{{$datalist->instruction_for_use}}</td>
-                <td>{{$datalist->instruction_for_others}}</td>
+                <td>{{$datalist->instruction_for_others}}</td> --}}
                 <td>
-                    <form>
-                        <button name="modify" value="{{$datalist->id}}">修改</button>
-                        <button name="delete" value="{{$datalist->id}}">刪除</button>
-                    </form>
+                    <button name="modify" value="{{$datalist->id}}">修改</button>
+                    <button name="delete" value="{{$datalist->id}}">刪除</button>
                 </td>
+                </form>
             </tr>
             @endforeach
         </tbody>
